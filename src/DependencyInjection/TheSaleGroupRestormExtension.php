b146b26b512944ca296ed03c55f54c8168fc49b3
Ryan <ryan.staniforth@thesalegroup.co.uk>
2017-11-30 10:39:01 +0000

    Pass event dispatcher on init

--- src/DependencyInjection/TheSaleGroupRestormExtension.php
+++ src/DependencyInjection/TheSaleGroupRestormExtension.php
@@ -27,7 +27,6 @@ namespace TheSaleGroup\RestormBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use TheSaleGroup\RestormBundle\DependencyInjection\Configuration;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
@@ -38,7 +37,6 @@ use Symfony\Component\Config\FileLocator;
  */
 class TheSaleGroupRestormExtension extends Extension
 {
-
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration;
@@ -50,5 +48,6 @@ class TheSaleGroupRestormExtension extends Extension
         
         $configurationDefinition = $container->getDefinition('thesalegroup.restorm.configuration');
         $configurationDefinition->addArgument($config);
+        $configurationDefinition->addArgument($container->get('event_dispatcher'));
     }
 }
